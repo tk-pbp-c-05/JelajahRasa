ab3d87bfc19de15312e271efd022ad7061c9b0d4
daffadesra <desrad81@gmail.com>
2024-10-09 13:58:04 +0700

    menambah allowed host

--- JelajahRasa/settings.py
+++ JelajahRasa/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-$gnzp(_!e$t+j-)uvltadvm=ywmgm2we)25*0=exktx8mw&)6c
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "daffa-desra-jelajahrasa.pbp.cs.ui.ac.id"]
 
 
 # Application definition
