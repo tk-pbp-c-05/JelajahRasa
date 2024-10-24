f3ecd294d71c33565a72f896fd5def1af13d06ee
kaylagrata <kayla.a.budiawan@gmail.com>
2024-10-24 11:37:30 +0700

    Add Login

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from main.views import register
+from main.views import register, login_user, logout_user, show_main
 
 app_name = 'main'
 
 urlpatterns = [
+    path('', show_main, name='show_main'),
     path('register/', register, name='register'),
+    path('login/', login_user, name='login'),
+    path('logout/', logout_user, name='logout'),
 ]
