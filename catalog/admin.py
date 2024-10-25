53d5f5a914da04c20cad731b89fc90ddb661c32a
satria <satria.aleka@gmail.com>
2024-10-25 22:49:37 +0700

    catalog

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,4 +1,3 @@
 from django.contrib import admin
-from main.models import Food
+
 # Register your models here.
-admin.site.register(Food)
--- main/models.py
+++ main/models.py
@@ -15,7 +15,7 @@ class Food(models.Model):
     flavor = models.CharField(max_length=100)
     category = models.CharField(max_length=50)
     vendor_name = models.CharField(max_length=100)  
-    price = models.IntegerField() 
+    price = models.CharField(max_length=255)
     map_link = models.CharField(max_length=255)
     address = models.CharField(max_length=255)
 
