c2b2f785b583893a9b5b526755990836da15e4c4
rafifsulaiman <rafifsulaimann@gmail.com>
2024-10-25 16:39:26 +0700

    modif again

--- module4/views.py
+++ module4/views.py
@@ -14,32 +14,27 @@ def add_dish(request):
     form = NewDishForm(request.POST, request.FILES or None)
     if form.is_valid() and request.method == 'POST':
         new_dish = form.save(commit=False)
-        # new_dish.user = request.user
+        new_dish.user = request.user
         
-        # # Jika admin, dish langsung di-approve
-        # if request.user.is_staff:  # Django default check for admin (staff)
-        #     new_dish.is_approved = True
-        #     new_dish.save()
-        #     messages.success(request, 'Dish added successfully!')
-        #     return redirect('module4:show_home')  # Redirect ke home setelah approve
-        # else:
-        #     # Jika user, dish belum di-approve dan disimpan untuk dicek oleh admin
-        #     new_dish.is_approved = False
-        #     new_dish.save()
-        #     messages.success(request, 'Dish added successfully! Please wait for our admin to approve it.')
-        #     return redirect('module4:show_home')  # Redirect ke halaman cek_dish
-        
-        new_dish.is_approved = False
-        new_dish.save()
-        messages.success(request, 'Dish added successfully! Please wait for our admin to approve it.')
-        return redirect('module4:show_home')
+        # Jika admin, dish langsung di-approve
+        if request.user.is_superuser:
+            new_dish.is_approved = True
+            new_dish.save()
+            messages.success(request, 'Dish added successfully!')
+            return redirect('module4:show_home')  # Redirect ke home setelah approve
+        else:
+            # Jika user, dish belum di-approve dan disimpan untuk dicek oleh admin
+            new_dish.is_approved = False
+            new_dish.save()
+            messages.success(request, 'Dish added successfully! Please wait for our admin to approve it.')
+            return redirect('module4:show_home')
     
     context = {"form": form}
     return render(request, 'add_dish.html', context)
 
 def check_dish(request):
-    # if not request.user.is_staff:
-    #     return redirect('module4:show_home')  # Hanya admin yang bisa mengakses halaman ini
+    if not request.user.is_staff:
+        return redirect('module4:show_home')  # Hanya admin yang bisa mengakses halaman ini
 
     # Tampilkan dish yang belum di-approve
     pending_dishes = NewDish.objects.filter(is_approved=False)
@@ -48,8 +43,8 @@ def check_dish(request):
     return render(request, 'check_dish.html', context)
 
 def approve_dish(request, dish_id):
-    # if not request.user.is_staff:
-    #     return redirect('module4:show_home')  # Hanya admin yang bisa melakukan ini
+    if not request.user.is_staff:
+        return redirect('module4:show_home')  # Hanya admin yang bisa melakukan ini
 
     dish = get_object_or_404(NewDish, id=dish_id)
 
