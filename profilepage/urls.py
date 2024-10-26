0380ad2f2b851fba2e62eb85448d4fda2f33a2a4
daffadesra <desrad81@gmail.com>
2024-10-26 20:24:48 +0700

    revise profile.html

--- profilepage/urls.py
+++ profilepage/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from profilepage.views import profile_view
+from profilepage.views import profile_view, edit_profile
 
 app_name = 'profilepage'
 
 urlpatterns = [
-    path('', profile_view, name='profile'),
+    path('<str:username>/', profile_view, name='profile'),
+    path('edit/<str:username>/', edit_profile, name='edit_profile'),
 ]
