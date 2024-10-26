082f051f4e3b71d53cd9dbcf84ce9c1567248877
Veynnz <diodavis123@gmail.com>
2024-10-26 20:07:04 +0700

    Updating register

--- main/models.py
+++ main/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser, BaseUserManager
 import uuid
+from django.db.models import Avg
 # Create your models here.
 
 class CustomUserManager(BaseUserManager):
@@ -66,31 +67,6 @@ class Food(models.Model):
     map_link = models.CharField(max_length=255)
     address = models.CharField(max_length=255)
     image = models.CharField(max_length=255, default="")
-    
-    def __str__(self):
-        return self.name
-
-from django.db import models
-from django.db.models import Avg
-from django.contrib.auth.models import AbstractUser
-
-# Create your models here.
-
-class CustomUser(AbstractUser):
-    email = models.EmailField(unique=True)  # Ensures unique email addresses
-    image_url = models.CharField(max_length=255, blank=True, null=True)  # Image field
-
-    def __str__(self):
-        return self.username
-
-class Food(models.Model):
-    name = models.CharField(max_length=100)
-    flavor = models.CharField(max_length=100)
-    category = models.CharField(max_length=50)
-    vendor_name = models.CharField(max_length=100)  
-    price = models.IntegerField() 
-    map_link = models.CharField(max_length=255)
-    address = models.CharField(max_length=255)
     rating_count = models.PositiveIntegerField(default=0)
 
     def update_average_rating(self):
@@ -101,7 +77,7 @@ class Food(models.Model):
             self.average_rating = avg_rating
             self.rating_count = count
             self.save()
-
+            
 class ProductRating(models.Model):
     food = models.ForeignKey(Food, on_delete=models.CASCADE, related_name='ratings')
     rating = models.PositiveIntegerField()
