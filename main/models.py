c40661b194ed43615a48dc4740313a7117c118b0
daffadesra <desrad81@gmail.com>
2024-10-27 22:24:30 +0700

    fix edit profile error

--- main/models.py
+++ main/models.py
@@ -33,6 +33,7 @@ class CustomUser(AbstractUser):
     image_url = models.CharField(max_length=255, blank=True, null=True)
     is_admin = models.BooleanField(default=False)
     location = models.CharField(max_length=50, blank=True, null=True, default="Indonesia")
+    description = models.TextField(max_length=255, blank=True, null=True, default="No description yet.")
     groups = models.ManyToManyField(
         'auth.Group',
         verbose_name='groups',
