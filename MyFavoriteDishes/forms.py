2f197e46816a2924e76b8953dcae9875850ebc34
kaylagrata <kayla.a.budiawan@gmail.com>
2024-10-27 21:45:42 +0700

    Add return string for food

--- MyFavoriteDishes/forms.py
+++ MyFavoriteDishes/forms.py
@@ -2,6 +2,15 @@ from django import forms
 from .models import FavoriteDish
 
 class FavoriteDishForm(forms.ModelForm):
+    FLAVOR_CHOICES = [
+        ('salty', 'Salty'),
+        ('sweet', 'Sweet'),
+    ]
+    
+    flavor = forms.ChoiceField(
+        choices=FLAVOR_CHOICES,
+        widget=forms.Select(attrs={'class': 'form-control'})
+    )
     class Meta:
         model = FavoriteDish
         fields = ['name', 'flavor', 'category', 'vendor_name', 'price', 'map_link', 'address']
